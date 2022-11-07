4c98d20295eb78e6966becaec3dea2287ccc8bae
Gondoor <goncacper@gmail.com>
2022-11-07 14:10:50 +0100

    Add project files.

---
+++ Server/Controllers/SuperHeroController.cs
@@ -0,0 +1,117 @@
+﻿using BlazorFullStackCrud.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorFullStackCrud.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
+        /*********************************************************************/
+        /* LIST all heroes from our database.
+         * we use iclude method to get as well all comics from another table */
+        /*********************************************************************/
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
+        {
+            var heroes = await _context.SuperHeroes.Include(sh => sh.Comic).ToListAsync();
+            return Ok(heroes);
+        }
+        /*************************************/
+        /* List all comics from our database */
+        /*************************************/
+        [HttpGet("comics")]
+        public async Task<ActionResult<List<Comic>>> GetComics()
+        {
+            var comics = await _context.Comics.ToListAsync();
+            return Ok(comics);
+        }
+
+        /********************************************************************/
+        /* List a single hero by id from our database
+         * We use iclude method to get as well all comics from another table*/
+        /********************************************************************/
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> GetSingleHero(int id)
+        {
+            var hero = await _context.SuperHeroes
+                .Include(hero => hero.Comic)
+                .FirstOrDefaultAsync(hero => hero.Id == id);
+            if (hero == null)
+            {
+                return BadRequest("Sorry no hero here. :/");
+            }
+            return Ok(hero);
+        }
+
+        /*********************************************/
+        /* CREATE a new SuperHeero into our database */
+        /*********************************************/
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> CreateSuperHero(SuperHero hero)
+        {
+            hero.Comic = null;
+            _context.SuperHeroes.Add(hero);
+            await _context.SaveChangesAsync();
+            return Ok(await GetDbHeroes());
+        }
+
+        /*********************************************/
+        /* List SuperHero from database  */
+        /*********************************************/
+        private async Task<List<SuperHero>> GetDbHeroes()
+        {
+            return await _context.SuperHeroes.Include(sh => sh.Comic).ToListAsync();
+        }
+
+        /*********************************************/
+        /* UPDATE a new SuperHeero into our database */
+        /*********************************************/
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> UpdateSuperHero(SuperHero hero, int id)
+        {
+            var dbHero = await _context.SuperHeroes
+                .Include(sh => sh.Comic)
+                .FirstOrDefaultAsync(sh => sh.Id == id);
+            if(dbHero == null)
+            {
+                return NotFound($"The SuperHero with id number {id} does not exist");
+            }
+
+            dbHero.FirstName = hero.FirstName;
+            dbHero.LastName = hero.LastName;
+            dbHero.HeroName = hero.HeroName;
+            dbHero.ComicId = hero.ComicId;
+
+            await _context.SaveChangesAsync();
+            return Ok(await GetDbHeroes());
+        }
+
+        /*********************************************/
+        /* DELETE a new SuperHeero into our database */
+        /*********************************************/
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteSuperHero( int id)
+        {
+            var dbHero = await _context.SuperHeroes
+                .Include(sh => sh.Comic)
+                .FirstOrDefaultAsync(sh => sh.Id == id);
+            if (dbHero == null)
+            {
+                return NotFound($"The SuperHero with id number {id} does not exist");
+            }
+
+            _context.SuperHeroes.Remove(dbHero);           
+
+            await _context.SaveChangesAsync();
+            return Ok(await GetDbHeroes());
+        }
+    }
+}
---
+++ Server/Data/DataContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BlazorFullStackCrud.Shared;
+using System.Data.Entity;
+
+namespace BlazorFullStackCrud.Server.Data
+{
+    public class DataContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Comic>().HasData(
+                  new Comic { Id = 1, Name = "Marvel" },
+                  new Comic { Id = 2, Name = "DC" }
+            );
+
+            modelBuilder.Entity<SuperHero>().HasData(
+                 new SuperHero
+                 {
+                     Id = 1,
+                     FirstName = "Peter",
+                     LastName = "Parker",
+                     HeroName = "Spiderman",                   
+                     ComicId = 1
+                 },
+                  new SuperHero
+                  {
+                      Id = 2,
+                      FirstName = "Bruce",
+                      LastName = "Wayne",
+                      HeroName = "Batman",                      
+                      ComicId = 2
+                  }
+             );
+        }
+
+        public Microsoft.EntityFrameworkCore.DbSet<SuperHero> SuperHeroes { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<Comic> Comics { get; set; }
+    }
+}
